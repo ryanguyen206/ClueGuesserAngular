4ce4bf3859220f15ba07c99458e8e1b44e90d119
ryanguyen206 <ryanninja595@gmail.com>
2023-10-27 10:19:00 -0700

    installed aos library for easy transitions

--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-tutorial',
@@ -10,6 +11,7 @@ export class TutorialComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init();
   }
 
 }
